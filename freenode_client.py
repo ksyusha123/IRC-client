042187aaad56bc441cd150762b8ce3c5da9a867f
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-10-20 22:39:18 +0500

    потоки

--- freenode_client.py
+++ freenode_client.py
@@ -1,27 +1,33 @@
 import socket
 import threading
 import sys
+import click
 
 
-def channel(channel):
-    if channel.startswith("#") == False:
-        return "#" + channel
-    return channel
+# def channel(channel):
+#     if channel.startswith("#") == False:
+#         return "#" + channel
+#     return channel
 
-# helper function used as thread target
-def print_response():
-    resp = client.get_response()
-    if resp:
-        msg = resp.strip().split(":")
-        print("< {}> {}".format(msg[1].split("!")[0], msg[2].strip()))
+# def print_response(client):
+#
 
 
-class IRCSimpleClient:
+class IRCClient:
     def __init__(self, username, channel, server="irc.libera.chat", port=6667):
         self.username = username
         self.server = server
         self.port = port
         self.channel = channel
+        self.conn = None
+        self.connect()
+        self.try_to_join_channel()
+
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()
+
+        write_thread = threading.Thread(target=self.process_commands)
+        write_thread.start()
 
     def connect(self):
         self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,55 +50,60 @@ class IRCSimpleClient:
         channel = self.channel
         self.send_cmd(cmd, channel)
 
+    def send_nick(self):
+        self.send_cmd("NICK", self.username)
+        self.send_cmd(
+            "USER", f"{self.username} * * :{self.username}")
+
+    def try_to_join_channel(self):
+        joined = False
+        while not joined:
+            resp = self.get_response()
+            print(resp.strip())
+            if "No Ident response" in resp:
+                self.send_nick()
+            if "376" in resp:
+                self.join_channel()
+            if "433" in resp:
+                self.username = f"_{self.username}"
+                self.send_nick()
+            if "PING" in resp:
+                self.send_cmd("PONG", ":" + resp.split(":")[1])
+            if "366" in resp:
+                joined = True
+
+    def process_commands(self):
+        cmd = input(f"<{self.username}> ").strip()
+        while cmd != "/quit":
+            self.send_message_to_channel(cmd)
+        self.send_cmd("QUIT", "Good bye!")
+
+    def receive(self):
+        resp = self.get_response()
+        while resp:
+            msg = resp.strip().split(":")
+            print(f"<{msg[1].split('!')[0]}> {msg[2].strip()}")
+
+
+@click.command()
+@click.argument('username')
+@click.argument('channel')
+def main(username, channel):
+    client = IRCClient(username, channel)
+
+
+    # while (cmd != "/quit"):
+    #     cmd = input("< {}> ".format(username)).strip()
+    #     if cmd == "/quit":
+    #         client.send_cmd("QUIT", "Good bye!")
+    #     client.send_message_to_channel(cmd)
+    #
+    #     # socket conn.receive blocks the program until a response is received
+    #     # to prevent blocking program execution, receive should be threaded
+    #     response_thread = threading.Thread(target=print_response)
+    #     response_thread.daemon = True
+    #     response_thread.start()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        usage()
-        exit(0)
-    else:
-        username = sys.argv[1]
-        channel = channel(sys.argv[2])
-
-    cmd = ""
-    joined = False
-    client = IRCSimpleClient(username, channel)
-    client.connect()
-
-    while(joined == False):
-        resp = client.get_response()
-        print(resp.strip())
-        if "No Ident response" in resp:
-            client.send_cmd("NICK", username)
-            client.send_cmd(
-                "USER", "{} * * :{}".format(username, username))
-
-        # we're accepted, now let's join the channel!
-        if "376" in resp:
-            client.join_channel()
-
-        # username already in use? try to use username with _
-        if "433" in resp:
-            username = "_" + username
-            client.send_cmd("NICK", username)
-            client.send_cmd(
-                "USER", "{} * * :{}".format(username, username))
-
-        # if PING send PONG with name of the server
-        if "PING" in resp:
-            client.send_cmd("PONG", ":" + resp.split(":")[1])
-
-        # we've joined
-        if "366" in resp:
-            joined = True
-
-    while(cmd != "/quit"):
-        cmd = input("< {}> ".format(username)).strip()
-        if cmd == "/quit":
-            client.send_cmd("QUIT", "Good bye!")
-        client.send_message_to_channel(cmd)
-
-        # socket conn.receive blocks the program until a response is received
-        # to prevent blocking program execution, receive should be threaded
-        response_thread = threading.Thread(target=print_response)
-        response_thread.daemon = True
-        response_thread.start()
+    main()
