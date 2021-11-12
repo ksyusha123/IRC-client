c91a26f6a4a24e5eaac66120c852550f13cb3ee7
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-11-13 00:21:35 +0500

    поправила интерфейс чата

--- chat_window.py
+++ chat_window.py
@@ -1,39 +1,55 @@
 import threading
 
-from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QTextEdit, \
-    QPushButton
+from PyQt5.QtWidgets import QWidget, QLabel, QTextEdit, \
+    QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout
 
 
 class ChatWindow(QWidget):
     def __init__(self, client):
         super(ChatWindow, self).__init__()
-
         self.client = client
-
-        grid = QGridLayout()
-        self.setLayout(grid)
-        messages_label = QLabel('Messages')
-        self.output_window = QTextEdit()
-        grid.addWidget(messages_label, 1, 2)
-        grid.addWidget(self.output_window, 2, 1, 2, 3)
-        self.input_window = QTextEdit()
-        self.input_window.setFocus()
+        self.output_field = self.create_output_field()
+        self.input_field = self.create_input_field()
         self.enter_button = QPushButton('Enter')
         self.enter_button.clicked.connect(self.send_data)
-        grid.addWidget(self.input_window, 4, 1, 4, 2)
-        grid.addWidget(self.enter_button, 4, 3, 6, 3)
+
+        self.init_layout()
 
         show_data_thread = threading.Thread(target=self.show_data)
         show_data_thread.start()
 
         self.show()
 
+    def init_layout(self):
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Messages"))
+        layout.addWidget(self.output_field)
+        layout.addLayout(self.create_input_field_layout())
+        self.setLayout(layout)
+
+    def create_input_field_layout(self):
+        layout = QHBoxLayout()
+        layout.addWidget(self.input_field)
+        layout.addWidget(self.enter_button)
+        return layout
+
+    def create_input_field(self):
+        input_field = QLineEdit()
+        input_field.returnPressed.connect(self.send_data)
+        input_field.setFocus()
+        return input_field
+
+    def create_output_field(self):
+        output_field = QTextEdit()
+        output_field.setReadOnly(True)
+        return output_field
+
     def send_data(self):
-        cmd = self.input_window.toPlainText()
+        cmd = self.input_field.text()
         self.client.process_commands(cmd)
-        self.output_window.insertPlainText(f"<ME> {cmd}\n")
-        self.input_window.clear()
+        self.output_field.insertPlainText(f"<ME> {cmd}\n")
+        self.input_field.clear()
 
     def show_data(self):
         for received_message in self.client.receive():
-            self.output_window.insertPlainText(f"{received_message}\n")
+            self.output_field.insertPlainText(f"{received_message}\n")
